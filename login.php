2b4769c741ec54946667abab8573bf5ba99a1e78
Kulsoom-Mateen <kulsoommateen07@gmail.com>
2020-04-12 11:39:59 -0400

    Added php file for login page

---
+++ login.php
@@ -0,0 +1,17 @@
+<DOCTYPE html>
+<html>
+    <head>
+        <title>Login Page</title>
+        <link href="style.css" rel="stylesheet">
+    </head>
+    <body style="background-color:rgba(110,130,240,0.3);">
+        <form class="box" action="" method="post">
+            <h5 style="color:black; margin-bottom: 50px;">Please sign in with your account</h3>
+            <input type="text" name="name" placeholder="Username" style="color:black;"><br><br>
+            <input type="password" name="password" placeholder="Password" style="color:black;"><br><br>
+            <input type="submit" name="" value="Login">
+            <h6 style="text-align: left;float:left;margin-top: 40px;"><a class="forgot" href="forgot_password.html" target="blank">Forgot password</a></h6>
+            <h6 style="text-align: right;margin-top: 45px;"><a class="join" href="signup.html" target="blank">Not registered,join now</a></h6>
+        </form>
+    </body>
+</html>
