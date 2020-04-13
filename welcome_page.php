f9c4f66b03a35a2b064cb4519d04e38652c1861d
Kulsoom-Mateen <kulsoommateen07@gmail.com>
2020-04-13 16:25:27 -0400

    Added php file for welcome page

---
+++ welcome_page.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>Welcome Page</title>
+</head>
+<body>
+    <div style="background-color:blue;color:white;text-align:center;font-size:100px;">Welcome to Home Page</div>
+</body>
+</html>
